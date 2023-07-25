618a9b072cdb7600af3cdbd13261f10f99c9ca4f
cristianoAutomation <tstlinux.c@gmaiil.com>
2023-07-25 00:57:57 -0300

    Primeiro commit

---
+++ CursoSelenium/src/main/java/TesteCampoTreinamento.java
@@ -0,0 +1,141 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TesteCampoTreinamento {
+
+	@Test
+	public void teste() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+	}
+
+	@Test
+	public void deveInteragirComTextField() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Meu primeiro teste");
+		Assert.assertEquals("Meu primeiro teste",
+				driver.findElement(By.id("elementosForm:nome")).getAttribute("value"));
+		driver.close();
+	}
+
+	@Test
+	public void deveInteragirComTextArea() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sugestoes")).sendKeys("Interagindo com TextArea");
+		String campoTextAreaPreenchido = driver.findElement(By.id("elementosForm:sugestoes")).getAttribute("value");
+		Assert.assertEquals("Interagindo com TextArea", campoTextAreaPreenchido);
+		driver.close();
+	}
+
+	@Test
+	public void deveInteragirComRadioButton() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:sexo:0")).isSelected());
+		driver.close();
+
+	}
+
+	@Test
+	public void deveInteragirComCheckBox() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("elementosForm:comidaFavorita:0")).click();
+		Assert.assertTrue(driver.findElement(By.id("elementosForm:comidaFavorita:0")).isSelected());
+		driver.close();
+
+	}
+
+	@Test
+	public void deveInteragirComCombox() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select select = new Select(element);
+		select.selectByVisibleText("Mestrado");
+		Assert.assertEquals("Mestrado", select.getFirstSelectedOption().getText());
+		driver.close();
+	}
+
+	@Test
+	public void deveVerificaValoresCombo() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:escolaridade"));
+		Select select = new Select(element);
+		List<WebElement> options = select.getOptions().get;
+		assertEquals(8, options.size());
+		
+		boolean encontrou = false;
+		
+		for (WebElement webElement : options) {
+			if(webElement.getText().equals("Mestrado")) {
+				encontrou = true;
+			}
+		}
+		Assert.assertTrue(encontrou);
+	
+		
+		driver.close();
+	}
+	
+	@Test
+	public void deveVerificaValoresComboMultipaEscolha() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		WebElement element = driver.findElement(By.id("elementosForm:esportes"));
+		Select select = new Select(element);
+		select.selectByVisibleText("Natacao");
+		select.selectByVisibleText("Corrida");
+		select.selectByVisibleText("O que eh esporte?");
+		List<WebElement> options = select.getAllSelectedOptions();
+		Assert.assertEquals(3, options.size());
+		
+		select.deselectByVisibleText("Corrida");
+		List<WebElement> optionsNovo = select.getAllSelectedOptions();
+		Assert.assertEquals(2, optionsNovo.size());
+		
+		driver.close();
+	}
+	
+	@Test
+	public void deveInteragirComBotao() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		driver.findElement(By.id("buttonSimple")).click();
+		String textBotãoClidado = driver.findElement(By.id("buttonSimple")).getAttribute("value");
+		Assert.assertEquals("Obrigado!", textBotãoClidado);
+		driver.close();
+	}
+	
+	@Test
+	public void deveInteragirComLink() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+	}
+
+}
---
+++ CursoSelenium/src/main/java/TesteGoogle.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+
+	@Test
+	public void teste() {
+
+		WebDriver driver = new FirefoxDriver();
+		// driver.manage().window().setPosition(new Point(100, 100));
+		// driver.manage().window().setSize(new Dimension(1200, 765));
+		// Primeiro parâmentro largura e depois altura
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com.br");
+		Assert.assertEquals("Google", driver.getTitle());
+		// valor atual vs valor esperado
+		driver.quit();
+	}
+
+	@Test
+	public void StartandoChromeDriver() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com.br");
+		Assert.assertEquals("Google", driver.getTitle());
+		// valor atual vs valor esperado
+		driver.quit();
+	}
+
+}
